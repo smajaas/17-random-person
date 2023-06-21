0bc6d881a112920880761722b312bfed98e78af4
smajaas <smajaas@gmail.com>
2023-06-21 16:27:10 +0530

    initial setup

---
+++ final/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
+function App() {
+  const [loading, setLoading] = useState(true);
+  return <h2>random user starter</h2>;
+}
+
+export default App;
